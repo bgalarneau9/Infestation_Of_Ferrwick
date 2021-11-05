ee1ed6ac6641f0994fa3b52ce4ce5dc19df112e9
Brandon Galarneau <bgalarneau3077@sdsu.edu>
2021-11-04 19:14:54 -0700

    Player character movement left and right in Hero_Controller

--- Assets/Scripts/Hero/Hero_Controller.cs
+++ Assets/Scripts/Hero/Hero_Controller.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Hero_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D rb;
+    public Vector2 moveBy;
+    public Animator anim;
+    public float moveSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveBy.x = Input.GetAxisRaw("Horizontal");
+        //moveBy.y = Input.GetAxisRaw("Vertical");
+        Debug.Log("X = " + moveBy.x + " Y = " + moveBy.y);
+        anim.SetFloat("Horizontal", moveBy.x);
+        //anim.SetFloat("Vertical", moveBy.y);
+        anim.SetFloat("speed", moveBy.sqrMagnitude);
+    }
+    private void FixedUpdate()
+    {
+        rb.position += moveBy * moveSpeed * Time.fixedDeltaTime;
     }
 }
+
