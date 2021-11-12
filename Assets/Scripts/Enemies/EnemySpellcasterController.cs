181419b16c917a18d079fdfbaeb762c4a4f1599c
Brandon Galarneau <bgalarneau3077@sdsu.edu>
2021-11-11 17:05:19 -0800

    Enemy spellcaster can now be killed

--- Assets/Scripts/Enemies/EnemySpellcasterController.cs
+++ Assets/Scripts/Enemies/EnemySpellcasterController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class EnemySpellcasterController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int health = 100;
+    [SerializeField]
+    private GameObject spellCasterPrefab;
+    [SerializeField]
+    private Transform _bloodSplatSpawn;
+    [SerializeField]
+    private GameObject _bloodSplatPrefab;
+
+    public AudioClip deathsound;
+
+    private AudioSource MyAudioSrc;
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "Projectile")
+        {
+            health -= 25;
+            if ( health <= 0)
+            {
+                GameObject bloodSplatter = Instantiate(_bloodSplatPrefab, _bloodSplatSpawn.position, Quaternion.identity);
+                bloodSplatter.GetComponent<ParticleSystem>().Play();
+                // 4 - Play Death sound
+                //MyAudioSrc.Play();
+                Destroy(spellCasterPrefab);
+            }
+        }
     }
 }
