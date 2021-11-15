c4f09aff1206d388028dc843ee198b74b4805a94
Brandon Galarneau <bgalarneau3077@sdsu.edu>
2021-11-15 14:35:33 -0800

    Camera only follows when player is alive. Z axis of camera locked

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,21 @@ public class CameraController : MonoBehaviour
 {
     private Transform player;
     private Vector3 offset;
+    private int numberOfPlayerPrefabs;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        //player.position = new Vector3(0, 0, -10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(player.position.x + offset.x, player.position.y + offset.y);
+        numberOfPlayerPrefabs = GameObject.FindGameObjectsWithTag("Player").Length;
+        if (numberOfPlayerPrefabs == 1)
+        {
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, -10);
+        }
     }
 }
