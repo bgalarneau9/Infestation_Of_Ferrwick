ccb6b35fb8bcebf7ff1b15235b0d8bbd63ea4b76
Brandon Galarneau <bgalarneau3077@sdsu.edu>
2021-11-22 15:39:00 -0800

    Goat runs from player at a fixed speed

--- Assets/Scripts/Enemies/EnemyGoatController.cs
+++ Assets/Scripts/Enemies/EnemyGoatController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EnemyGoatController : MonoBehaviour
 {
     //Attacking Player
+    private GameObject player;
+    private Vector3 playerPosition;
+    private float distBetween;
     private float allowableAttackDist;
     private float timeBetweenAttack;
     private float nextAttack;
@@ -36,13 +39,15 @@ public class EnemyGoatController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         rb.constraints = RigidbodyConstraints2D.FreezePositionY;
         health = 250;
-        moveSpeed = 1;
+        moveSpeed = 0.10f;
         timeBetweenAttack = 4;
         allowableAttackDist = 5;
         nextAttack = Time.time + timeBetweenAttack;
+        moveBy = new Vector2(-1, 0);
     }
 
     // Update is called once per frame
@@ -52,19 +57,39 @@ public class EnemyGoatController : MonoBehaviour
         {
             tryAttack();
         }
+        int numberOfPlayer = GameObject.FindGameObjectsWithTag("Player").Length;
+        if (numberOfPlayer > 0)
+        {
+            distBetween = Vector3.Distance(player.transform.position, enemyPrefab.transform.position);
+            tryMove();
+        }
+        rb.rotation = 0;
     }
     private void tryAttack()
     {
         GameObject hero = GameObject.FindGameObjectWithTag("Player");
         if (Time.time >= nextAttack)
         {
-            anim.SetBool("isAttacking", true);
+            anim.Play("Goat_Attack");
             var projectile = Instantiate(EnemyProjectilePrefab, EnemyProjectileSpawnPoint.position, Quaternion.identity) as GameObject;
             var projectileRigidBody = projectile.GetComponent<Rigidbody2D>();
             projectileRigidBody.velocity = Quaternion.Euler(0, 0, 0) * Vector3.left * 15; //10 == power
             Destroy(projectile, 2f);
             nextAttack = Time.time + timeBetweenAttack;
             anim.SetTrigger("Idle");
+        }
+    }
+    private void tryMove()
+    {
+        if (distBetween > 3)
+        {
+            anim.Play("Goat_Move_Left");
+            rb.position += moveBy * moveSpeed * Time.fixedDeltaTime;
+        }
+        else if (distBetween < 2)
+        {
+            anim.Play("Goat_Move_Right");
+            rb.position -= moveBy * 3 * Time.fixedDeltaTime;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
