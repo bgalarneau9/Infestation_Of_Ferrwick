5e23b3f5e697a191b02471e765861a50a6b04285
Brandon Galarneau <bgalarneau3077@sdsu.edu>
2021-11-22 11:18:17 -0800

    Projectiles now ignore skeleton patrol routes

--- Assets/Scripts/Combat/ProjectileController.cs
+++ Assets/Scripts/Combat/ProjectileController.cs
@@ -18,6 +18,10 @@ public class ProjectileController : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.tag == "FlipMovement")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<BoxCollider2D>(), heroPrefab.GetComponent<BoxCollider2D>());
+        }
         Destroy(projectile);
     }
 }
